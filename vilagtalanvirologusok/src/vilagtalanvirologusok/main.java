bd31b2b3b8e90f564a7a8cb491f3bbb282ce39e2
piewbond <64149375+piewbond@users.noreply.github.com>
2022-03-24 15:49:44 +0100

    hello world

--- vilagtalanvirologusok/src/vilagtalanvirologusok/main.java
+++ vilagtalanvirologusok/src/vilagtalanvirologusok/main.java
@@ -1,10 +1,11 @@
 package vilagtalanvirologusok;
 
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
 	}
 
 }
