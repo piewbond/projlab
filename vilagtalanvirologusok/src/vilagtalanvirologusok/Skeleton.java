67d307269991a32e85e7355c5acf8498e031772f
Kovács Ádám <kovacsadam23@edu.bme.hu>
2022-03-26 19:07:12 +0100

    Update SkeletonMenu()

--- vilagtalanvirologusok/src/vilagtalanvirologusok/Skeleton.java
+++ vilagtalanvirologusok/src/vilagtalanvirologusok/Skeleton.java
@@ -4,25 +4,35 @@ import java.util.Scanner;
 
 public class Skeleton {
     public void SkeletonMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("[0]Virologist moves\n" +
-                "[1]Virologist crafts agent\n" +
-                "[2]Virologist use protector vaccine\n" +
-                "[3]Virologist use paralyze virus\n" +
-                "[4]Virologist use chorea virus\n" +
-                "[5]Virologist use amnesia virus\n" +
-                "[6]Virologist picks up material\n" +
-                "[7]Virologist use glove\n" +
-                "[8]Virologist use cloak\n" +
-                "[9]Virologist learns genetic code\n" +
-                "[10]Virologist picks up equipment\n" +
-                "[11]Virologist steal equipment\n" +
-                "[12]Virologist ends turn\n" +
-                "[13]Virologist affected by chorea virus\n");
+                "[1]  Virologist crafts agent\n" +
+                "[2]  Virologist use protector vaccine\n" +
+                "[3]  Virologist use paralyze virus\n" +
+                "[4]  Virologist use chorea virus\n" +
+                "[5]  Virologist use amnesia virus\n" +
+                "[6]  Virologist picks up material\n" +
+                "[7]  Virologist use glove\n" +
+                "[8]  Virologist use cloak\n" +
+                "[9]  Virologist learns genetic code\n" +
+                "[10] Virologist picks up equipment\n" +
+                "[11] Virologist steal equipment\n" +
+                "[12] Virologist ends turn\n" +
+                "[13] Virologist affected by chorea virus\n");
 
-        System.out.println();
-        VirologistMoves(1);
+        String selected = scanner.nextLine();
+
+        switch (selected) {
+            case "1":
+                VirologistMoves();
+            case "2":
+                VirologistUseProtectorVaccine();
+            case "3":
+                VirologistUseParalyzeVirus();
+            case "4":
+        };
     }
-    public void VirologistMoves(int scenarionmb){
+    public void VirologistMoves(){
         int callnmb = 1;
         prt(callnmb++,"Virologist","v","Move()","");
         prt(callnmb++,"Street","s","GetNeighbours()","list<Neighbours> neighbours");
@@ -53,7 +63,7 @@ public class Skeleton {
         };
 
     }
-    public void VirologistUseParalyzeVirus(int scenarionmb){
+    public void VirologistUseParalyzeVirus(){
 
     }
     public void VirologistUseChoreaVirus()
@@ -64,25 +74,25 @@ public class Skeleton {
     {
 
     }
-    public void VirologistPicksUpMaterial(int scenarionmb){
+    public void VirologistPicksUpMaterial(){
 
     }
-    public void VirologistUseGlove(int scenarionmb){
+    public void VirologistUseGlove(){
 
     }
-    public void VirologistUseCloak(int scenarionmb){
+    public void VirologistUseCloak(){
 
     }
-    public void VirologistLearnsGeneticCode(int scenarionmb){
+    public void VirologistLearnsGeneticCode(){
 
     }
-    public void VirologistPicksUpEquipment(int scenarionmb){
+    public void VirologistPicksUpEquipment(){
 
     }
-    public void VirologistStealEquipment(int scenarionmb){
+    public void VirologistStealEquipment(){
 
     }
-    public void VirologistEndsTurn(int scenarionmb){
+    public void VirologistEndsTurn(){
 
     }
     public void VirologistAffectedByChoreaVirus()
